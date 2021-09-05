3e5cc9b3aa4c7de25a5469b59fae7740fc8dfd94
User01 <user01@gmail.com>
2021-09-05 16:32:57 +0700

    first commit

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1630833480,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg1-sheet0.png",
+		"images/bgmap-sheet0.png",
+		"images/boy-sheet0.png",
+		"images/boy-sheet1.png",
+		"images/gamepad-sheet0.png",
+		"images/gamepad-sheet1.png",
+		"images/control-sheet0.png",
+		"images/block-sheet0.png",
+		"images/knife-sheet0.png",
+		"images/knife-sheet1.png",
+		"images/gamepad2-sheet0.png",
+		"images/gamepad2-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
